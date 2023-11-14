e4a688732d6ee357ff0ed4c47fd93bdffc97a820
Stanislav Filippov <stanislav.filippov@gmail.com>
2023-11-14 14:15:13 -0500

    Added basic LLM class

--- app.py
+++ app.py
@@ -2,3 +2,106 @@
 
 import gradio as gr
 import time
+import logging
+import datetime as dt
+import sys
+
+from src.models.load import LLM
+
+def echo(message, history, system_prompt, tokens, T):
+    response = f"Temperature: {T}\nSystem prompt: {system_prompt}\n Message: {message}."
+    for i in range(min(len(response), int(tokens))):
+        time.sleep(0.05)
+        yield response[: i+1]
+
+
+def start_server(*args):
+    SERVER_PORT = 5050
+
+    def my_function(x, progress=gr.Progress()):
+        progress(0, desc="Starting...")
+        time.sleep(1)
+        for i in progress.tqdm(range(100)):
+            time.sleep(0.1)
+        return x
+
+    with gr.Blocks() as demo:
+        system_prompt = gr.Textbox("You are helpful AI.", label="System Prompt")
+        T = gr.Number(label="Temperature", value=0.5, interactive=True, render=False)
+        with gr.Row() as row1:
+            top_p = gr.Number(label="Top p", value=0.85, interactive=True, render=True)
+            do_sample = gr.Checkbox(value=True, interactive=True, label='Do sampling', render=True)
+
+        #with gr.Row():
+        #    T = gr.Number(label="Temperature", value=0.5, interactive=True, render=False)
+        #    do_sample = gr.Checkbox(value=True, interactive=True, label='Do sampling', render=False)
+
+        slider = gr.Slider(25, 250, render=False)
+
+        gr.Interface(my_function, gr.Textbox(), gr.Textbox())
+
+        gr.ChatInterface(
+            echo,
+            additional_inputs=[system_prompt, slider, T]
+            #additional_inputs=[system_prompt, slider]
+        )
+
+    demo.queue().launch(server_port=SERVER_PORT)
+
+def set_logger():
+    """ Sets up a stdout logger """
+    today = dt.datetime.today()
+    dt_str = f"{today.month:02d}-{today.day:02d}-{today.year}"
+
+    logFormatter = logging.Formatter(
+        fmt="[%(asctime)s] [%(name)8s] [%(levelname)-8s] %(message)s"
+    )
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setFormatter(logFormatter)
+    ch.setLevel(logging.DEBUG)
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    root.addHandler(ch)
+    logger = logging.getLogger(__name__)
+    return logger
+
+
+##########################################################################################################
+if __name__ == '__main__':
+    import json
+    import argparse
+
+    arg_desc = '''\
+        Config file location. Required parameter!
+        '''
+
+    logger = set_logger()
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=arg_desc)
+
+    parser.add_argument("-cfg", metavar="config file", help="Path to the config", required=True)
+    args = parser.parse_args()
+    cfg_file = args.cfg
+
+    msg = """
+    ===================================================================
+        Starting a simple local LLM chatbot wrapper for experiments.
+    =================================================================== 
+    """
+    logger.info(msg)
+    with open(cfg_file, 'r') as f:
+        config = json.load(f)
+
+    if 'model' not in config:
+        logger.critical(f'Config file does not contain model config!')
+        sys.exit(1)
+
+    llm = LLM(config['model'])
+    llm.load_base_model()
+    #start_server()
+
+
+
+
--- src/models/load.py
+++ src/models/load.py
@@ -1,9 +1,126 @@
-import torch
+import sys
 
+
+import json
+import logging
+
+import torch
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     BitsAndBytesConfig
 )
-
 from peft import PeftConfig
+
+def verify_config(cfg):
+    """ A simple check of the model config """
+    logger = logging.getLogger(__name__)
+    if 'base_model' not in cfg:
+        logger.critical('No base model provided!')
+        return False
+    elif cfg['base_model'] is None or cfg['base_model'] == '':
+        logger.critical('Base model is empty!')
+        return False
+    return True
+
+
+def get_4bit_bnb_config(cfg):
+    """ Builds simple 4 bits BnB quantization config """
+    ans = {}
+    ans['bnb_4bit_quant_type'] = cfg['quantization'].get('bnb_4bit_quant_type', 'nf4')
+    ans['bnb_4bit_use_double_quant'] = cfg['quantization'].get('bnb_4bit_use_double_quant', True)
+    dtype = cfg['quantization'].get('bnb_4bit_compute_dtype', 'bf16').lower()
+    if dtype=='bf16':
+        dtype = torch.bfloat16
+    elif dtype=='fp16':
+        dtype = torch.float16
+    else:
+        dtype = None
+    ans['dtype'] = dtype
+
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type=ans['bnb_4bit_quant_type'],
+        bnb_4bit_compute_dtype= ans['dtype'],
+        bnb_4bit_use_double_quant=ans['bnb_4bit_use_double_quant']
+    )
+
+    return bnb_config
+
+
+def parse_config(cfg):
+    """ Parses the model config """
+    ans = {}
+    logger = logging.getLogger(__name__)
+    ans['base_model'] = cfg['base_model']
+
+    _t = cfg.get('adapter', None)
+    ans['adapter'] = _t if _t != '' else None
+    msg = f'Using adapter.' if ans['adapter'] else 'Adapter is not provided.'
+    logger.info(msg)
+
+    _t = cfg.get('cache_dir', None)
+    ans['cache_dir'] = _t if _t != '' else None
+    msg = f'Using cache dir: {ans["cache_dir"]}' if ans['cache_dir'] else 'Using default cache dir.'
+    logger.info(msg)
+
+    if 'quantization' in cfg:
+        logger.info('Building simple 4 bits BnB config')
+        ans['quant'] = get_4bit_bnb_config(cfg)
+    else:
+        logger.info('No quantization for loading')
+        ans['quant'] = None
+
+    ans['dev_map'] = cfg.get('device_map', 'auto')
+    _t = cfg.get('torch_dtype', None)
+    if _t:
+        if _t.lower() == 'fp16':
+            _t = torch.float16
+        elif _t.lower() == 'bf16':
+            _t = torch.bfloat16
+        else:
+            _t = 'auto'
+    ans['load_dtype'] = _t
+    return ans
+
+class LLM:
+    def __init__(self, config):
+        """ Parses the config """
+        self.logger = logging.getLogger(__name__)
+        if not verify_config(config):
+            self.logger.critical('Config is not correct!')
+            sys.exit(-1)
+        else:
+            self.logger.info(f'Config is OK')
+        self.cfg = parse_config(config)
+
+    def load_base_model(self):
+        """ Loads the base model """
+        self.model = AutoModelForCausalLM.from_pretrained(
+            self.cfg['base_model'],
+            cache_dir=self.cfg['cache_dir'],
+            quantization_config=self.cfg['quant'],
+            device_map=self.cfg['dev_map'],
+            torch_dtype=self.cfg['load_dtype']
+        )
+
+    def load_tokenizer(self):
+        """ Loads tokenizer """
+        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=self.cfg['base_model'],
+                                          cache_dir=self.cfg['cache_dir'],  padding_size="left")
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+
+    def add_adapter(self):
+        """ Adds adapter """
+        if self.cfg['adapter']:
+            peft_config2 = PeftConfig.from_pretrained(self.cfg['adapter'])
+            self.model.add_adapter(peft_config2)
+    def enable_adapter(self):
+        """ Enables adapter """
+        if self.cfg['adapter']:
+            self.model.enable_adapters()
+
+    def disable_adapter(self):
+        """ Disables adapter """
+        if self.cfg['adapter']:
+            self.model.enable_adapters()
