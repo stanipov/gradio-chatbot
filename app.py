140047d85de2fd36f848c9ed7f0d3012ecf3e8e7
Stanislav Filippov <stanislav.filippov@gmail.com>
2023-11-15 20:05:25 -0500

    Prototype of working streaming in Gradio with custom model. Many things to change

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from functools import partial
 
 from src.model import Mistral_chat_LLM
 from src.utils import merge2ChatML
+from threading import Thread
 
 def echo(message, history, system_prompt, tokens, T):
     response = f"Temperature: {T}\nSystem prompt: {system_prompt}\n Message: {message}."
@@ -16,44 +17,95 @@ def echo(message, history, system_prompt, tokens, T):
         time.sleep(0.05)
         yield response[: i+1]
 
-def generate_core(llm, message, history, system_prompt,
-                  top_p, top_k, T, penalty_alpha,
-                  max_new_tokens, do_sample,
-                  output_type):
-    # form ChatML
+def __generate_core(message, history, llm, system_prompt, output_type):
+    # Generate prompt
+    logger = logging.getLogger(__name__)
+    #logger.debug(f'Message: {message}')
+    #logger.debug(f'History: {history}')
+    #logger.debug(f'System prompt: {system_prompt}')
+    messages = merge2ChatML(message, history, system_prompt, )
+    prompt = llm.conv_messages_prompt(messages)
+    #logger.debug(f'Prompt: {prompt}')
+    response = llm.generate(prompt=prompt, kind=output_type)
+    return response
+
+
+def generate_core(message, history, system_prompt, llm):
+    # Generate prompt
     messages = merge2ChatML(message, history, system_prompt)
+    prompt = llm.conv_messages_prompt(messages)
+
+    llm.logger.debug(f'PROMPT: {prompt}')
+    tokenized_prompt = llm.tokenizer([prompt], return_tensors="pt").to(llm.device)
+    llm.logger.debug(f'TOKENIZED PROMPT: {tokenized_prompt}')
+    streamer_kw = dict(
+        tokenized_prompt,
+        streamer=llm.streamer,
+        max_new_tokens=llm.max_new_tokens,
+        do_sample=llm.do_sample,
+        top_p=llm.top_k,
+        top_k=llm.top_k,
+        temperature=llm.T,
+        num_beams=llm.num_beams
+    )
+    t = Thread(target=llm.model.generate, kwargs=streamer_kw)
+    t.start()
+
+    partial_message = ""
+    for new_token in llm.streamer:
+        if new_token != llm.bos_tok or new_token != llm.eos_tok:
+            partial_message += new_token
+            yield partial_message
 
 
 
-    pass
-
-
-def start_server(llm, server_port: int=5050, output_type:str='streaming'):
+def start_server(llm, server_port: int = 5050):
+    logger = logging.getLogger(__name__)
 
     with gr.Blocks() as demo:
-        system_prompt = gr.Textbox("You are helpful AI.", label="System Prompt")
+        _system_prompt = gr.Textbox("You are helpful AI chatbot.", label="System Prompt", interactive=True)
+        system_prompt = _system_prompt.value
+        with gr.Accordion(label='Parameters', open=False) as acc1:
+            with gr.Row() as row1:
+                top_p = float(gr.Number(label="Top p", value=0.95, interactive=True, render=True).value)
+                top_k = int(gr.Number(label="Top k", value=250, interactive=True, render=True).value)
+                T = float(gr.Number(label="Temperature", value=0.5, interactive=True, render=True).value)
+                num_beams = int(gr.Number(label="Beams", value=1, interactive=True, render=True).value)
+                penalty_alpha = float(gr.Number(label="Repetition penalty", value=1.5, interactive=True, render=True).value)
+                max_new_tokens = int(gr.Number(label="# new tokens", value=512, interactive=True, render=True).value)
+                do_sample = bool(gr.Checkbox(value=True, interactive=True, label='Do sampling', render=True).value)
+                base_llm = bool(gr.Checkbox(value=False, interactive=True, label='Base LLM', render=True).value)
+                output_type = str(gr.Dropdown(interactive=True, label='Output behavior', value='streaming',
+                                               render=True, choices=['streaming', 'pipe']).value)
+        logger.debug(f'Output behavior: {output_type}')
+        logger.debug(f'max_new_tokens: {max_new_tokens}')
 
-        with gr.Row() as row1:
-            top_p = gr.Number(label="Top p", value=0.95, interactive=True, render=True)
-            top_k = gr.Number(label="Top k", value=250, interactive=True, render=True)
-            T = gr.Number(label="Temperature", value=0.5, interactive=True, render=False)
-            penalty_alpha = gr.Number(label="Repetition penalty", value=1.0, interactive=True, render=True)
-            max_new_tokens = gr.Number(label="# new tokens", value=1024, interactive=True, render=True)
-            do_sample = gr.Checkbox(value=True, interactive=True, label='Do sampling', render=True)
+        # unset/set adapter
+        if base_llm:
+            llm.disable_adapter()
+        else:
+            llm.enable_adapter()
 
-        generate = partial(generate_core,
-                           llm=llm,
-                           system_prompt=system_prompt,
-                           top_p=top_p,
-                           top_k=top_k,
-                           T=T,
-                           penalty_alpha=penalty_alpha,
-                           max_new_tokens=max_new_tokens,
-                           do_sample=do_sample,
-                           output_type=output_type
-                           )
+        # set up generation part:
+        if output_type=='streaming':
+            llm.set_streamer(T, top_k, top_p, do_sample, num_beams, max_new_tokens, penalty_alpha)
+        elif output_type=='pipe':
+            llm.set_gen_pipeline(T, top_k, top_p, do_sample, num_beams, max_new_tokens, penalty_alpha)
+        else:
+            logger.warning(f'Output behavior "{output_type}" is not recognized. Rolling back to streaming.')
+            output_type == 'streaming'
+            llm.set_streamer(T, top_k, top_p, do_sample, num_beams, max_new_tokens, penalty_alpha)
+
+        #generate = partial(generate_core,
+        #                   llm=llm,
+        #                   system_prompt=system_prompt,
+        #                   output_type=output_type
+        #                   )
+
+
         gr.ChatInterface(
-            echo
+            generate_core,
+            additional_inputs=[llm, system_prompt]
         )
 
     demo.queue().launch(server_port=server_port)
@@ -117,9 +169,52 @@ if __name__ == '__main__':
     out_behavior = config['app'].get('output_behavior', 'streaming')
 
     llm = Mistral_chat_LLM(config['model'])
-    llm.load_base_model()
-    start_server(llm, serv_port, out_behavior)
+    llm.from_pretrained(enable_adapter=True)
 
+    #start_server(llm, serv_port, out_behavior)
+    with gr.Blocks() as demo:
+        _system_prompt = gr.Textbox("You are helpful AI chatbot.", label="System Prompt", interactive=True)
+        system_prompt = _system_prompt.value
+        with gr.Accordion(label='Parameters', open=False) as acc1:
+            with gr.Row() as row1:
+                top_p = float(gr.Number(label="Top p", value=0.95, interactive=True, render=True).value)
+                top_k = int(gr.Number(label="Top k", value=250, interactive=True, render=True).value)
+                T = float(gr.Number(label="Temperature", value=0.5, interactive=True, render=True).value)
+                num_beams = int(gr.Number(label="Beams", value=1, interactive=True, render=True).value)
+                penalty_alpha = float(
+                    gr.Number(label="Repetition penalty", value=1.5, interactive=True, render=True).value)
+                max_new_tokens = int(gr.Number(label="# new tokens", value=512, interactive=True, render=True).value)
+                do_sample = bool(gr.Checkbox(value=True, interactive=True, label='Do sampling', render=True).value)
+                base_llm = bool(gr.Checkbox(value=False, interactive=True, label='Base LLM', render=True).value)
+                output_type = str(gr.Dropdown(interactive=True, label='Output behavior', value='streaming',
+                                              render=True, choices=['streaming', 'pipe']).value)
+        logger.debug(f'Output behavior: {output_type}')
+        logger.debug(f'max_new_tokens: {max_new_tokens}')
+
+        # unset/set adapter
+        if base_llm:
+            llm.disable_adapter()
+        else:
+            llm.enable_adapter()
+
+        # set up generation part:
+        if output_type == 'streaming':
+            llm.set_streamer(T, top_k, top_p, do_sample, num_beams, max_new_tokens, penalty_alpha)
+        elif output_type == 'pipe':
+            llm.set_gen_pipeline(T, top_k, top_p, do_sample, num_beams, max_new_tokens, penalty_alpha)
+        else:
+            logger.warning(f'Output behavior "{output_type}" is not recognized. Rolling back to streaming.')
+            output_type == 'streaming'
+            llm.set_streamer(T, top_k, top_p, do_sample, num_beams, max_new_tokens, penalty_alpha)
+
+        generate = partial(generate_core, llm=llm)
+
+        gr.ChatInterface(
+            generate,
+            additional_inputs=[_system_prompt]
+        )
+
+    demo.queue().launch(server_port=serv_port)
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,11 @@
+import logging
+import torch
+from transformers import BitsAndBytesConfig
+##################################  Code  ##################################
+
 def isClose(a,b, eps=1e-4):
     """ Compares 2 float numbers if they are close """
     return abs(a-b) <=eps
-
 
 def merge2ChatML(message, history, sys_prompt):
     """ Merges user message, history, and system prompt into ChatML format """
@@ -12,3 +16,74 @@ def merge2ChatML(message, history, sys_prompt):
 
     messages.append({'role': 'assistant', 'content': message})
     return messages
+
+
+def verify_config(cfg):
+    """ A simple check of the model config """
+    logger = logging.getLogger(__name__)
+    if 'base_model' not in cfg:
+        logger.critical('No base model provided!')
+        return False
+    elif cfg['base_model'] is None or cfg['base_model'] == '':
+        logger.critical('Base model is empty!')
+        return False
+    return True
+
+##################################  Parse config for LLM  ##################################
+def get_4bit_bnb_config(cfg):
+    """ Builds simple 4 bits BnB quantization config """
+    ans = {}
+    ans['bnb_4bit_quant_type'] = cfg['quantization'].get('bnb_4bit_quant_type', 'nf4')
+    ans['bnb_4bit_use_double_quant'] = cfg['quantization'].get('bnb_4bit_use_double_quant', True)
+    dtype = cfg['quantization'].get('bnb_4bit_compute_dtype', 'bf16').lower()
+    if dtype=='bf16':
+        dtype = torch.bfloat16
+    elif dtype=='fp16':
+        dtype = torch.float16
+    else:
+        dtype = None
+    ans['dtype'] = dtype
+
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type=ans['bnb_4bit_quant_type'],
+        bnb_4bit_compute_dtype= ans['dtype'],
+        bnb_4bit_use_double_quant=ans['bnb_4bit_use_double_quant']
+    )
+
+    return bnb_config
+
+def parse_config(cfg):
+    """ Parses the model config """
+    ans = {}
+    logger = logging.getLogger(__name__)
+    ans['base_model'] = cfg['base_model']
+
+    _t = cfg.get('adapter', None)
+    ans['adapter'] = _t if _t != '' else None
+    msg = f'Using adapter.' if ans['adapter'] else 'Adapter is not provided.'
+    logger.info(msg)
+
+    _t = cfg.get('cache_dir', None)
+    ans['cache_dir'] = _t if _t != '' else None
+    msg = f'Using cache dir: {ans["cache_dir"]}' if ans['cache_dir'] else 'Using default cache dir.'
+    logger.info(msg)
+
+    if 'quantization' in cfg:
+        logger.info('Building simple 4 bits BnB config')
+        ans['quant'] = get_4bit_bnb_config(cfg)
+    else:
+        logger.info('No quantization for loading')
+        ans['quant'] = None
+
+    ans['dev_map'] = cfg.get('device_map', 'auto')
+    _t = cfg.get('torch_dtype', None)
+    if _t:
+        if _t.lower() == 'fp16':
+            _t = torch.float16
+        elif _t.lower() == 'bf16':
+            _t = torch.bfloat16
+        else:
+            _t = 'auto'
+    ans['load_dtype'] = _t
+    return ans
